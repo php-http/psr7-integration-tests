c30bcb13998ed1d903be93896506a3fc6ed5ab8d
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2023-04-04 15:48:40 +0200

    feature: introduce `userInfo` encoding tests
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- src/UriIntegrationTest.php
+++ src/UriIntegrationTest.php
@@ -281,4 +281,28 @@ abstract class UriIntegrationTest extends BaseTest
     {
         $this->assertSame($test['expected'], (string) $test['uri']);
     }
+
+    /**
+     * Tests that special chars in `userInfo` must always be URL-encoded to pass RFC3986 compliant URIs where characters
+     * in username and password MUST NOT contain reserved characters.
+     *
+     * This test is taken from {@see https://github.com/guzzle/psr7/blob/3cf1b6d4f0c820a2cf8bcaec39fc698f3443b5cf/tests/UriTest.php#L679-L688 guzzlehttp/psr7}.
+     *
+     * @see https://www.rfc-editor.org/rfc/rfc3986#appendix-A
+     */
+    public function testSpecialCharsInUserInfo(): void
+    {
+        $uri = $this->createUri('/')->withUserInfo('foo@bar.com', 'pass#word');
+        self::assertSame('foo%40bar.com:pass%23word', $uri->getUserInfo());
+    }
+
+    /**
+     * Tests that userinfo which is already encoded is not encoded twice.
+     * This test is taken from {@see https://github.com/guzzle/psr7/blob/3cf1b6d4f0c820a2cf8bcaec39fc698f3443b5cf/tests/UriTest.php#L679-L688 guzzlehttp/psr7}.
+     */
+    public function testAlreadyEncodedUserInfo(): void
+    {
+        $uri = $this->createUri('/')->withUserInfo('foo%40bar.com', 'pass%23word');
+        self::assertSame('foo%40bar.com:pass%23word', $uri->getUserInfo());
+    }
 }
