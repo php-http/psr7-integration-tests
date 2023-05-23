92b65deddf2265ad16b2c45bde28334d294d5c71
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-23 17:42:10 +0300

    fix

--- tests/HttpSoft/RequestTest.php
+++ tests/HttpSoft/RequestTest.php
@@ -6,11 +6,17 @@ namespace Http\Psr7Test\Tests\HttpSoft;
 
 use Http\Psr7Test\RequestIntegrationTest;
 use HttpSoft\Message\Request;
+use HttpSoft\Message\Uri;
 
 class RequestTest extends RequestIntegrationTest
 {
     public function createSubject()
     {
         return new Request('GET', '/');
+    }
+
+    protected function buildUri($uri)
+    {
+        return new Uri($uri);
     }
 }
